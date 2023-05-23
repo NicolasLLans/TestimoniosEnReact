86a922192dbb1994174f377fa40dc9726c8f28bc
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-05-23 00:08:06 -0300

    cree los props para el componente testimonio

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../hojasDeEstilos/Testimonios.css'
 
-function Testimonio(){
+function Testimonio(props){
   return (
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio'
-      src={require('../imagenes/testimonio-emma.png')}
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
       alt='imagen de emma'/>
-      <div className='contenedor-tecto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bositan en Suecia</p>
-        <p className='cargo-testimonio'>Ingenieria de software en Spotify</p>
-        <p className='texto.testimonio'>"Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
